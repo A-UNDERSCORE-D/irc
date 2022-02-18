6fa9078e36787c77b147b840363c834563f6381c
A_D <aunderscored@gmail.com>
2022-02-18 09:08:49 +0200

    add argument.Replyf helper

--- event/chatcommand/argument.go
+++ event/chatcommand/argument.go
@@ -1,6 +1,7 @@
 package chatcommand
 
 import (
+	"fmt"
 	"strings"
 
 	"awesome-dragon.science/go/irc/event"
@@ -22,4 +23,9 @@ type Argument struct {
 // ArgString returns the arguments as a space joined string
 func (a *Argument) ArgString() string {
 	return strings.Join(a.Arguments, " ")
+}
+
+// Replyf is a thin wrapper around reply that allows for easy printf formatting of replies
+func (a *Argument) Replyf(format string, args ...interface{}) {
+	a.Reply(fmt.Sprintf(format, args...))
 }
